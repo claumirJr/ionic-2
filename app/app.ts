0632adeef16ab39e717d30c42851eb4fd4882b7d
Jr <claumir.junior@hotmail.com>
2016-10-24 13:51:01 -0200

    capitulo03-aula04

--- app/app.ts
+++ app/app.ts
@@ -2,20 +2,34 @@ import {Component} from '@angular/core';
 import {Platform, ionicBootstrap} from 'ionic-angular';
 import {StatusBar} from 'ionic-native';
 import {HomePage} from './pages/home/home';
+import {MenuTestPage} from './pages/meun-test/meun-test';
 
 
 @Component({
-  template: '<ion-nav [root]="rootPage"></ion-nav>'
+  templateUrl: 'build/app.html'
 })
 export class MyApp {
+
+  pages: Array<{component: any, title: string, icon: string}>;
+
   rootPage: any = HomePage;
 
   constructor(platform: Platform) {
+
+    this.pages = [
+      {component: HomePage, title: 'Home', icon: 'home'},
+      {component: MenuTestPage, title: 'Menu Teste', icon: 'menu'}
+    ];
+
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       StatusBar.styleDefault();
     });
+  }
+
+  openPage(page: any) : void {
+    this.rootPage = page.component;
   }
 }
 
---
+++ app/pages/meun-test/meun-test.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+/*
+  Generated class for the MeunTestPage page.
+
+  See http://ionicframework.com/docs/v2/components/#navigation for more info on
+  Ionic pages and navigation.
+*/
+@Component({
+  templateUrl: 'build/pages/meun-test/meun-test.html',
+})
+export class MenuTestPage {
+  constructor(private nav: NavController) {}
+}
